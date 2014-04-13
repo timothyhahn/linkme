c4fa334c3279461e308af55d3529adf436f39547
Timothy Hahn <timyhahn@gmail.com>
2014-04-12 23:25:50 -0400

    Added heroku stuff

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn linkme:app
--- linkme/settings.py
+++ linkme/settings.py
@@ -1,3 +1,7 @@
-DATABASE_PATH = 'sqlite:///linkme.db'
+import os
+if os.environ.get('DATABASE_URL'):
+    DATABASE_PATH = os.environ['DATABASE_URL']
+else:
+    DATABASE_PATH = 'sqlite:///linkme.db'
 SECRET_KEY = 'secret'
 DEBUG = True 
